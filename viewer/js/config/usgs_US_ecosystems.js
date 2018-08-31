9948e1a18c97c26ba05c6586af18e2a173b5ee5f
hoogw hu <hoogw@hotmail.com>
2018-08-31 11:40:07 -0700

    usgs done

---
+++ viewer/js/config/usgs_US_ecosystems.js
@@ -0,0 +1,196 @@
+define([
+    'esri/units',
+    'esri/geometry/Extent',
+    'esri/config',
+    /*'esri/urlUtils',*/
+    'esri/tasks/GeometryService',
+    'esri/layers/ImageParameters',
+    'gis/plugins/Google',
+    'dojo/i18n!./nls/main',
+    'dojo/topic',
+    'dojo/sniff'
+], function (units, Extent, esriConfig, /*urlUtils,*/ GeometryService, ImageParameters, GoogleMapsLoader, i18n, topic, has) {
+
+    
+    // https://developers.google.com/maps/documentation/javascript/get-api-key
+    GoogleMapsLoader.KEY = 'xxxxxxxxxxxxx';
+
+    
+    
+
+    return {
+        // used for debugging your app
+        isDebug: true,
+
+        //default mapClick mode, mapClickMode lets widgets know what mode the map is in to avoid multipult map click actions from taking place (ie identify while drawing).
+        defaultMapClickMode: 'identify',
+        // map options, passed to map constructor. see: https://developers.arcgis.com/javascript/jsapi/map-amd.html#map1
+        mapOptions: {
+            basemap: 'streets',
+            center: [-117.246521, 34.049039],
+            zoom: 5,
+            sliderStyle: 'small'
+        },
+
+        
+
+        // custom titles
+        titles: {
+            header: i18n.viewer.titles.header,
+            subHeader: i18n.viewer.titles.subHeader,
+            pageTitle: i18n.viewer.titles.pageTitle
+        },
+
+        layout: {
+            
+        },
+
+        
+        // operationalLayers: Array of Layers to load on top of the basemap: valid 'type' options: 'dynamic', 'tiled', 'feature'.
+        // The 'options' object is passed as the layers options for constructor. Title will be used in the legend only. id's must be unique and have no spaces.
+        // 3 'mode' options: MODE_SNAPSHOT = 0, MODE_ONDEMAND = 1, MODE_SELECTION = 2
+        operationalLayers: 
+        [
+
+
+            {
+                type: 'dynamic',
+                //type: 'feature',
+                url: 'https://wildfire.cr.usgs.gov/arcgis/rest/services/contUS/MapServer',
+                title: 'USGS United States ecosystems',
+                
+
+                layerControlLayerInfos: {
+            
+                    // layer control options
+                    noLegend: false,
+                    noZoom: false,
+                    swipe: true,
+                    swipeScope: true,
+                    expanded: true,
+                    sublayers: true,
+                    metadataUrl: false,
+                    allSublayerToggles	: true,
+                   
+                }
+
+
+
+            }, 
+    
+
+
+
+
+
+        ],
+
+
+
+
+
+
+
+
+        // set include:true to load. For titlePane type set position the the desired order in the sidebar
+        widgets: {
+
+            layerControl: {
+                include: true,
+                id: 'layerControl',
+                type: 'titlePane',
+                path: 'gis/dijit/LayerControl',
+                title: i18n.viewer.widgets.layerControl,
+                iconClass: 'fa-th-list',
+                open: true,
+                position: 0,
+                options: {
+
+                    map: true,
+                    
+                    
+                    layerControlLayerInfos: true,
+                    
+                    
+
+
+                }
+            },
+
+
+
+
+
+
+            search: {
+                include: true,
+                type: 'domNode',
+                path: 'esri/dijit/Search',
+                srcNodeRef: 'geocoderButton',
+                title: i18n.viewer.widgets.search,
+                iconClass: 'fa-search',
+                position: 0,
+                options: {
+                    map: true,
+                    visible: true,
+                    enableInfoWindow: false,
+                    enableButtonMode:  true,
+                    expanded:  false
+                }
+            },
+
+
+
+            basemaps: {
+                include: true,
+                id: 'basemaps',
+                type: 'domNode',
+                path: 'gis/dijit/Basemaps',
+                srcNodeRef: 'basemapsDijit',
+                options: 'config/basemaps'
+            },
+
+
+
+            identify: {
+                include: true,
+                id: 'identify',
+                type: 'titlePane',
+                path: 'gis/dijit/Identify',
+                title: i18n.viewer.widgets.identify,
+                iconClass: 'fa-info-circle',
+                open: false,
+                preload: true,
+                position: 3,
+                options: 'config/identify'
+            },
+
+
+          
+
+            legend: {
+                include: true,
+                id: 'legend',
+                type: 'titlePane',
+                path: 'gis/dijit/Legend',
+                title: i18n.viewer.widgets.legend,
+                iconClass: 'fa-picture-o',
+                open: false,
+                position: 1,
+                options: {
+                    map: true,
+                    legendLayerInfos: true
+                }
+            },
+
+
+
+            
+
+
+
+
+        }
+    };
+});
+
